dab02222b1d3408f3b8812bca52de94f5d04e8d8
ShahinHsynzd <69747173+ShahinHsynzd@users.noreply.github.com>
2021-04-17 15:19:53 +0400

    Wrapped and position fixed

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const navigation = document.querySelector('nav');
 const btnOpen = document.querySelector('.fa-bars');
 const btnClose = document.querySelector('.fa-times');
+
 btnOpen.addEventListener('click', function () {
     navigation.style.right = '0';
 })
